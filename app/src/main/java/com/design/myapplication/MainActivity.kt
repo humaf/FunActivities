4242b563da7d04d4455b8e2df17335a778df8ed3
red <huma.winkel@gmail.com>
2020-06-23 11:24:57 -0700

    Added all the functinality

--- app/src/main/java/com/design/myapplication/MainActivity.kt
+++ app/src/main/java/com/design/myapplication/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.design.myapplication
 
 
-import android.app.Dialog
+
 import android.content.Context
-import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
@@ -11,16 +10,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.ContextThemeWrapper
-import android.view.LayoutInflater
-import android.view.View
-import android.view.Window
-import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.design.myapplication.model.Item
 import com.design.myapplication.network.MyApi
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -32,12 +26,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-   //    var result :String = getData()
-
-   //     Log.i("vvvvvvvvvvvv",result)
-       // getData()
-
-
         btn.setOnClickListener {
            // text.setVisibility(View.VISIBLE)
             if(isNetworkConnected()) {
@@ -48,43 +36,20 @@ class MainActivity : AppCompatActivity() {
             }
                 else
             {
-                AlertDialog.Builder(this).setTitle("No Internet Connection")
+
+                AlertDialog.Builder(this,R.style.AlertDialogTheme).setTitle("No Internet Connection")
                     .setMessage("Please check your internet connection and try again")
                     .setPositiveButton(android.R.string.ok) { _, _ -> }
                     .setIcon(android.R.drawable.ic_dialog_alert).show()
 
             }
 
-            // card.setVisibility(View.VISIBLE)
-
-     /*       val builder = AlertDialog.Builder(this)
-
-            with(builder)
-            {
-                setTitle("Hello")
-                setMessage(result)
-                setPositiveButton("OK", null)
-                show()
-            }
-
-
-      */
 
         }
-
-        okbtn.setOnClickListener {
-            card.setVisibility(View.GONE)
-            val refreshActivity = intent
-            finish()
-            startActivity(refreshActivity)
-        }
-
     }
 
 
    private fun getData() : String {
-
-  //  private fun getData(){
 
         var res = ""
 
@@ -103,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                 val items = response.body().toString()
 
                 val domain: String = items.substringAfterLast("=")
-                res = domain.toString()
+                res = domain.dropLast(1)
                 Log.i("rrrrrrrrrrrrrrr",res)
 
                 items?.let {
@@ -135,68 +100,23 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-    private fun showDialog(title: String) {
-        val dialog = Dialog(applicationContext)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.dialog_layout)
-      //  val body = dialog.findViewById(R.id.dialogText) as TextView
-      //  body.text = title
-        val yesBtn = dialog.findViewById(R.id.dialogButton) as Button
-     //   val noBtn = dialog.findViewById(R.id.noBtn) as TextView
-        yesBtn.setOnClickListener {
-            dialog.dismiss()
-        }
-    //    noBtn.setOnClickListener { dialog.dismiss() }
-        dialog.show()
-
-    }
-
     private fun showd(message : String){
 
         //    val builder = AlertDialog.Builder(this)
 
-   /*     val builder = AlertDialog.Builder(ContextThemeWrapper(this, R.style.AlertDialogTheme))
+       val builder = AlertDialog.Builder(ContextThemeWrapper(this, R.style.AlertDialogTheme))
         builder.setPositiveButton("Okay",null)
-
+        builder.setMessage(message)
         // Create the AlertDialog
         val alertDialog: AlertDialog = builder.create()
 
         // Set other dialog properties
         alertDialog.setCancelable(false)
         alertDialog.show()
-*/
-
-       val Dialog = MaterialAlertDialogBuilder(this,R.style.AlertDialogTheme)
-           .setMessage(message)
-           .setPositiveButton("Okay",null)
-           .show();
-
 
     }
 
 
-    fun showMessageBox(text: String){
-
-        //Inflate the dialog as custom view
-        val messageBoxView = LayoutInflater.from(applicationContext).inflate(R.layout.dialog_layout, null)
-
-        //AlertDialogBuilder
-        val messageBoxBuilder = AlertDialog.Builder(applicationContext).setView(messageBoxView)
-
-        //setting text values
-    //  dialogText.setText(text)
-
-        //show dialog
-        val  messageBoxInstance = messageBoxBuilder.show()
-
-        //set Listener
-        messageBoxView.setOnClickListener(){
-            //close dialog
-            messageBoxInstance.dismiss()
-        }
-    }
 }
 
 
